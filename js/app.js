5c6dee3db4758d0c70312cb130cc5eaa002dc2b6
Manuel Rueda <manuel.rueda.un@gmail.com>
2015-11-19 20:08:03 -0300

    first functional version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-(function(window){
+(function(window, document, Prism, ngAnnotate){
 
-})(window);
+  document.querySelector('#originalCode').value
+
+  document.querySelector('#resultCode').textContent
+
+  document.querySelector('#process').addEventListener('click', function(){
+    document.querySelector('#resultCode').textContent = ngAnnotate(document.querySelector('#originalCode').value, {add:true}).src;
+    Prism.highlightElement(document.querySelector('#resultCode'));
+  });
+
+})(window, window.document, Prism, ngAnnotate);
